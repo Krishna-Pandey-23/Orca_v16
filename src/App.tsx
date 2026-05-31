c38e526be5f15d7734b593e84c939e00bb845f6d
Krishna-Pandey-23 <sheetalshiksha2312@gmail.com>
2026-05-31 18:59:36 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -786,7 +786,7 @@ export default function App() {
       )}
 
       {/* LEFT SIDE NAVIGATION PANEL BAR */}
-      <aside className="hidden md:flex flex-col h-full py-6 px-6 glass-panel glass-panel-silver-border w-72 shrink-0 z-30">
+      <aside className="hidden md:flex flex-col py-6 px-6 glass-panel glass-panel-silver-border w-72 shrink-0 z-30">
         <div className="mb-2 px-2 flex justify-center">
           <img src={OrcaLogo} alt="Orca Logo" className="w-10 h-10 object-contain" />
         </div>
